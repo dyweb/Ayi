8e3d486ef3c52d0ba72048abc5f206c6681632e3
at15 <at15@outlook.com>
2015-08-08 23:50:15 +1000

    Move check.go to lib folder
    - change the package to check
    Otherwise `go install` would have error

--- Ayi.go
+++ Ayi.go
@@ -2,45 +2,6 @@ package main
 
 import "fmt"
 
-func weekend() (string, string) {
-	return  "Saturday", "Sunday"
-}
-
 func main() {
-	var Ayi = "xyp"
-	x := 1 + 1
-	const c = "I am constant"
-	fmt.Println(Ayi)
-	fmt.Println("1 + "  + "1 = ", x)
-
-	for i := 1; i <= 3; i++ {
-		fmt.Println(i)
-	}
-
-	if 1 < 2 {
-		fmt.Println("1 < 2")
-	}
-
-	// array have fixed length
-	var arr [2]int
-	for j := 0; j < len(arr); j++ {
-		fmt.Println(arr[j])
-	}
-
-	// slice
-	var s []int
-	s = make([]int ,2)
-	s = append(s,3,2)
-
-	for k := 0; k < len(s); k++ {
-		fmt.Println(s[k])
-	}
-
-	// map
-	var m = make(map[string]string)
-
-	m["ayi"] = "xyp"
-
-	sat, sun := weekend()
-	fmt.Println(sat,sun)
+	fmt.Println("Hello Mie!")
 }
