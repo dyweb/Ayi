6e54b1821320882b4318dfe0e5be9c3d53837d82
Ce Gao <gaocegege@hotmail.com>
2016-02-28 01:41:46 +0800

    mock version

---
+++ app/mail/mail.go
@@ -0,0 +1,11 @@
+package mail
+
+import (
+	"github.com/codegangsta/cli"
+	// "github.com/mailgun/mailgun-go"
+)
+
+// SendMailToWebStuff will send the mail to web stuff group
+func SendMailToWebStuff(c *cli.Context) {
+
+}
