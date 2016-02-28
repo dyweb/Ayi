b2ba941f41d6556f40a80364acc919a81b5dada4
Ce Gao <gaocegege@hotmail.com>
2016-02-28 15:39:50 +0800

    update: first version of the mail service

--- app/mail/mail.go
+++ app/mail/mail.go
@@ -1,8 +1,8 @@
 package mail
 
 import (
-	"errors"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/codegangsta/cli"
 	"github.com/dyweb/Ayi/common"
@@ -15,12 +15,27 @@ func SendMailToWebStuff(c *cli.Context) {
 	domain := getMailgunConfigString(common.MailgunDomain)
 	prikey := getMailgunConfigString(common.MailgunPrikey)
 	pubkey := getMailgunConfigString(common.MailgunPubkey)
-
+	sender := getMailgunConfigString(common.Sender)
+	email := c.Args().Get(0)
+	subject := c.Args().Get(1)
+	fileName := c.Args().Get(2)
+	
+	
+	// validate the args and config of mailgun
 	if validateConfig(domain, prikey, pubkey) == false {
-		panic(errors.New("Mailgun error: config needed."))
+		fmt.Println("Mailgun error: config needed.")
+		cli.ShowCommandHelp(c, "send")
+		return
 	}
+	if validateArgs(email, subject, fileName) == false {
+		fmt.Println("Mailgun error: wrong args.")
+		cli.ShowCommandHelp(c, "send")
+		return
+	}
+	
 	gun := mailgun.NewMailgun(domain, prikey, pubkey)
-	m := mailgun.NewMessage("Sender <sender@example.com>", "Subject", "Message Body", "Recipient <recipient@example.com>")
+	m := mailgun.NewMessage(sender, subject, "nil", fmt.Sprintf("Web stuff <%s>", email))
+	m.SetHtml(readHTMLFile(fileName))
 	response, id, _ := gun.Send(m)
 	fmt.Printf("Response ID: %s\n", id)
 	fmt.Printf("Message from server: %s\n", response)
@@ -35,4 +50,22 @@ func validateConfig(domain string, prikey string, pubkey string) bool {
 		return false
 	}
 	return true
+}
+
+func validateArgs(email string, subject string, fileName string) bool {
+	fmt.Println(subject, fileName)
+	if email == "" || fileName == "" || subject == "" {
+		return false
+	}
+	return true
+}
+
+func readHTMLFile(fileName string) string {
+	
+
+	b, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Errorf("Disk I/O error: %s", err)
+	}
+	return fmt.Sprintf("%s", b)
 }
