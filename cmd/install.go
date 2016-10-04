60eaa3e5749edf522486c4d8bc3f67a80f3f8594
at15 <at15@outlook.com>
2016-10-03 21:36:04 -0700

    [CMD] Move runner to sub dir
    - add commands lookup, which should support both array and string (NOT
      tested)
    - use the new runner in install command

--- cmd/install.go
+++ cmd/install.go
@@ -1,10 +1,11 @@
 package cmd
 
 import (
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	"os"
 
-	"github.com/dyweb/Ayi/util"
+	"github.com/spf13/cobra"
+
+	"github.com/dyweb/Ayi/util/runner"
 )
 
 var installCmd = &cobra.Command{
@@ -12,15 +13,11 @@ var installCmd = &cobra.Command{
 	Short: "build and install binary",
 	Long:  "build and install binary following commands defined in install block in .ayi.yml",
 	Run: func(cmd *cobra.Command, args []string) {
-		hasInstall := viper.IsSet("install")
-		if !hasInstall {
-			log.Warn("Install configuration not found!")
-			return
-		}
-		commands := viper.GetStringSlice("install")
-		for _, cmd := range commands {
-			log.Infof("executing: %s \n", cmd)
-			util.RunCommand(cmd)
+		_, err := runner.ExecuteCommand("install")
+		if err != nil {
+			log.Error(err.Error())
+			log.Error("install failed")
+			os.Exit(1)
 		}
 	},
 }
