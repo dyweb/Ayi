72e29cd173a406279f6388f5651318484fa49a05
at15 <at15@outlook.com>
2018-04-27 00:25:44 -0700

    [app][github] List issue labels of a repo
    - add example label config from @at15
    - introduce golang.org/x/oauth2 for using go-github
      - might write a http transport wrapper in gommon to avoid this
    dependency, go-github just need a http client that can inject header
    https://github.com/dyweb/gommon/issues/70
    - the label is hard coded to at15/papers-i-read

--- app/github/app.go
+++ app/github/app.go
@@ -1,15 +1,22 @@
 package github
 
 import (
+	"context"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
+
 	"github.com/dyweb/Ayi"
 	dlog "github.com/dyweb/gommon/log"
+	"github.com/google/go-github/github"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 type App struct {
 	root *cobra.Command
 	r    Ayi.Registry
+	c    *github.Client
 
 	log *dlog.Logger
 }
@@ -33,9 +40,13 @@ func NewApp(r Ayi.Registry) (*App, error) {
 		Short: "manage label",
 		Long:  "Manage GitHub issue labels",
 		Run: func(cmd *cobra.Command, args []string) {
-			a.r.HasHomeConfig()
-			a.r.HomeConfig()
-			a.log.Infof("you token is %s", a.r.HomeConfig().GitHub.Token)
+			a.createClient()
+			labels, _, err := a.c.Issues.ListLabels(context.Background(), "at15", "papers-i-read", nil)
+			if err != nil {
+				a.log.Fatal(err)
+				return
+			}
+			a.log.Infof("labels are %s", labels)
 		},
 	}
 	root.AddCommand(label)
@@ -45,4 +56,38 @@ func NewApp(r Ayi.Registry) (*App, error) {
 
 func (a *App) CobraCommand() *cobra.Command {
 	return a.root
+}
+
+// TODO: move it to gommon as stringutil
+func maskSensitive(s string) string {
+	l := len(s)
+	if l < 2 {
+		return "*"
+	}
+	s = s[:2]
+	for i := 0; i < l-2; i++ {
+		s += "*"
+	}
+	return s
+}
+
+// TODO: move it to gommon requests
+// NOTE: base on https://github.com/golang/oauth2/blob/master/oauth2.go#L314
+// FIXME: should have a way to not use oauth2 package ...
+func oauthHttpClient(token string) *http.Client {
+	return oauth2.NewClient(context.Background(), oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token}))
+}
+
+func (a *App) createClient() {
+	if !a.r.HasHomeConfig() {
+		a.log.Fatal("~/.ayi.yml not found, required for github token")
+		return
+	}
+	token := a.r.HomeConfig().GitHub.Token
+	if token == "" {
+		a.log.Fatal("github token is empty")
+		return
+	}
+	a.log.Debugf("you token is %s", maskSensitive(token))
+	a.c = github.NewClient(oauthHttpClient(token))
 }
---
+++ app/github/label.go
@@ -0,0 +1,7 @@
+package github
+
+// import (
+// 	"github.com/google/go-github/github"
+// )
+
+// func
